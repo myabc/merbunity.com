e2923619694019021d1b9ff65d51786d14571560
Daniel Neighman <has.sox@gmail.com>
2008-03-07 23:57:09 +1100

    Adds specs for Merbunity::Permissions::ProtectedModel

--- lib/permissions/protected_model.rb
+++ lib/permissions/protected_model.rb
@@ -2,6 +2,30 @@ module Merbunity
   module Permissions
     module ProtectedModel
       
+      def viewable_by?(user)
+        if self.respond_to?(:owner)
+          return self.owner == user ? true : false
+        else
+          true
+        end
+      end
+      
+      def editable_by?(user)
+        if self.respond_to?(:owner)
+          return self.owner == user ? true : false
+        else
+          true
+        end
+      end
+      
+      def destroyable_by?(user)
+        if self.respond_to?(:owner)
+          return self.owner == user ? true : false
+        else
+          true
+        end
+      end
+      
     end
   end
 end
