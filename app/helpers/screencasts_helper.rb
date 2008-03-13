b24e9863e9d7d220eb5795ec353b0b18ac3ba68b
Daniel Neighman <has.sox@gmail.com>
2008-03-14 01:02:38 +1100

    Changes the screencast section menu for logged in and publisher stuff

--- app/helpers/screencasts_helper.rb
+++ app/helpers/screencasts_helper.rb
@@ -2,9 +2,11 @@ module Merb
     module ScreencastsHelper
       def section_menu_items
         items = []
-        items << link_to("My Pending",  url(:my_pending_screencasts))   
-        items << link_to("All Pending", url(:pending_screencasts))
-        items << link_to("Drafts",      url(:drafts_screencasts))
+        if logged_in?
+          items << link_to("My Pending",  url(:my_pending_screencasts))   
+          items << link_to("All Pending", url(:pending_screencasts)) if current_person.admin? || current_person.publisher?
+          items << link_to("Drafts",      url(:drafts_screencasts))
+        end
         items
       end
     end
