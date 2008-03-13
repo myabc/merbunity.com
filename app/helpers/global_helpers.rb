6a223e6c7456403ffa51b4bc67310d83459ebf09
Daniel Neighman <has.sox@gmail.com>
2008-03-13 18:10:06 +1100

    Adds truncate to string and some partials

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -8,5 +8,26 @@ module Merb
     def page_title
       @_page_title
     end
+    
+    def for_publishers
+      yield if current_person.publisher? || current_person.admin?
+    end
+    
+  end
+end
+
+class String
+  # Truncates a string to the last previous end of word and applies a suffix.
+  def truncate(required_length = 50, suffix = "...")
+    if self.size > required_length
+      # return self[0...required_length] << "..."
+      if self.include?(" ")
+        return self.slice(/\A(.){0,#{required_length}}\b/, 0).strip << suffix
+      else
+        return self.slice(0...required_length) << suffix
+      end
+    else
+      return self
+    end
   end
 end
---
+++ spec/helpers/global_helpers_spec.rb
@@ -0,0 +1,39 @@
+require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
+
+describe Merb::GlobalHelpers, "String#truncate" do
+  
+  it "should not truncate the string if the string is not longer than the given parameter" do
+    [5,10,15].each do |i|
+      s = "a" * i
+      s.truncate(16).size.should == i
+    end   
+  end
+  
+  it "should truncate a string and put '...' on the end" do
+    a = "abc def ghi jkl mno pqr"
+    a.truncate(15).should == "abc def ghi jkl..."    
+  end
+  
+  it "should truncate a string and put '...' at the closest previous word boundary" do
+    a = "abc def ghi jkl mno pqr"
+    a.truncate(17).should == "abc def ghi jkl..."
+  end
+  
+  it "should put a different suffix on it if I want" do
+    a = "abc def"
+    a.truncate(6, "!!!").should == "abc!!!"
+  end
+  
+  it "should truncate the string if there are no spaces" do
+    a = "abcdefghijk"
+    a.truncate(3).should == "abc..."
+  end
+  
+  it "should not change the string when it truncates" do
+    a = "abcdef"
+    a.truncate(2)
+    a.should == "abcdef"
+  end
+  
+  
+end
