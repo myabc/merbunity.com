8241ec4bebc281e0875f2de9bda2ef1429ca48e3
Daniel Neighman <has.sox@gmail.com>
2008-04-29 23:29:09 +1000

    Changes some stuff

--- app/controllers/pending_features.rb
+++ app/controllers/pending_features.rb
@@ -2,24 +2,20 @@ class PendingFeatures < Application
   
   before :set_page_header
   
-  def news
-    render "Keep an eye out.  The news feature isn't far away"
-  end
-  
   def tutorials
-    render "Keep an eye out. There's going to be some great tutorials soon"
+    render "<div class='item'>Keep an eye out. There's going to be some great tutorials soon</div>"
   end
   
   def blogs
-    render "Keep and eye out.  We'll be adding a blog register shortly"
+    render "<div class='item'>Keep and eye out.  We'll be adding a blog register shortly</div>"
   end
   
   def sites
-    render "Want to put your merb site up to showcase.  This section will be done shortly."
+    render "<div class='item'>Want to put your merb site up to showcase.  This section will be done shortly.</div>"
   end
   
   def projects
-    render "Show off your projects, coming soon"
+    render "<div class='item'>Show off your projects, coming soon</div>"
   end
   
   protected
