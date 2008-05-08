8b35890a39bbad0a321944452739ca9349cd1029
Daniel Neighman <has.sox@gmail.com>
2008-05-09 00:13:28 +1000

    Adds TOS, FAQ and credit links

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,18 @@
 $( function() { 
     $( 'code' ).chili(); //try to comment this line to see the difference 
+
+  	// Set the display for definition lists
+		$('dt').click(function(event){
+			$(event.target).next("dd").slideToggle();
+		});
+		
+		// Set the style of the DT and stuff under it.
+		$('dl')
+			  .children('dd')
+				.css({display : 'none'})
+				.end()
+				.children('dt')
+				.css({ "text-decoration" : "underline", cursor : "pointer"});  
+
+
 } );
