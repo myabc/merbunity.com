5b31a4f4bd5b533a8b674e30b0214ec0ec26fac2
Daniel Neighman <has.sox@gmail.com>
2008-05-09 02:33:22 +1000

    Adds style to the object menu, and js to the delete button to confirm

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,13 @@
 $( function() { 
     $( 'code' ).chili(); //try to comment this line to see the difference 
 
+		// set a confirm on delete buttons
+		$('.delete button').click(function(){
+			if(!confirm("You cannot undo this delete.\nYou will lose all information.\nAre you sure you want to continue?")){
+				return false;
+			}
+		});
+			
   	// Set the display for definition lists
 		$('dt').click(function(event){
 			$(event.target).next("dd").slideToggle();
