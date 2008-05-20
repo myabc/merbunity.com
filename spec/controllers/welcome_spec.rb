53932ad36275eebb7094fed0b646a3332fa9d994
Daniel Neighman <has.sox@gmail.com>
2008-05-20 22:22:37 +1000

    Adds a welcome controller

---
+++ spec/controllers/welcome_spec.rb
@@ -0,0 +1,69 @@
+require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
+
+describe Welcome do
+  before(:all) do
+    day = Merb::Const::DAY
+    @publisher = Person.create(valid_person_hash)
+    @publisher.activate
+    @publisher.make_publisher!
+    
+    @person = Person.create(valid_person_hash)
+    @person.activate
+    
+    1.upto(12) do |i|
+      s = Screencast.new(valid_screencast_hash.with(:owner => @publisher))
+      s.save
+      s.publish!(@publisher)
+      s.created_at = Time.now - (day * i)
+      s.published_on = Time.now - (day * i)
+      s.save
+      
+      s = Tutorial.new(valid_tutorial_hash.with(:owner => @publisher))
+      s.save
+      s.publish!(@publisher)
+      s.created_at = Time.now - (day * i)
+      s.published_on = Time.now - (day * i)
+      s.save
+      
+      n = NewsItem.create(valid_news_item_hash.with(:owner => @publisher))
+      n.created_at = Time.now - (day * i)
+      n.save
+    end 
+    
+    @result = [Screencast.published(:limit => 10), Tutorial.published(:limit => 10), NewsItem.all(:limit => 10)].flatten
+    @result = @result.sort_by{|item| item.respond_to?(:published_on) ? item.published_on : item.created_at }
+    @result = @result[0,10]
+    
+    @sc_result =  Screencast.published( :limit => 4)
+    @ni_result =  NewsItem.all(   :limit => 4)
+    @tut_result = Tutorial.published(   :limit => 4)
+    
+  end
+  
+  describe "index action" do
+    def do_index(opts = {})
+      @controller = dispatch_to(Welcome, :index, opts)
+    end
+    
+    before(:each) do
+      do_index
+    end
+    
+    it "should be successful" do
+      @controller.should be_successful
+    end
+    
+    it "should have the latest 4 screencasts" do
+      @controller.assigns(:screencasts).should == @sc_result      
+    end
+    
+    it "should have the latest 4 News Items" do
+      @controller.assigns(:news_items).should == @news_items      
+    end
+    
+    it "should have the latest 4 tutorials""" do
+      @controller.assigns(:tutorials).should == @tutorials
+    end
+    
+  end
+end
