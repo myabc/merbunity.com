9e01366eb4d46222265411ce2e246c254087ec88
Daniel Neighman <has.sox@gmail.com>
2008-05-23 01:30:11 +1000

    Changes around the welcome page to seperate news, but mixin tutorials and screencasts

--- spec/controllers/welcome_spec.rb
+++ spec/controllers/welcome_spec.rb
@@ -34,9 +34,11 @@ describe Welcome do
     @feed_result = @feed_result.sort_by{|item| item.respond_to?(:published_on) ? item.published_on : item.created_at }.reverse
     @feed_result = @feed_result[0,10]
     
-    @sc_result =  Screencast.published( :limit => 4)
-    @ni_result =  NewsItem.all(   :limit => 4)
-    @tut_result = Tutorial.published(   :limit => 4)
+    @recent_items = [Screencast.published(:limit => 5), Tutorial.published(:limit => 5)].flatten
+    @recent_items = @recent_items.sort_by{|item| item.published_on }.reverse
+    @recent_items = @recent_items[0,5]
+    
+    @news_items = NewsItem.all(:order => "created_at DESC", :limit => 3)
     
   end
   
@@ -52,17 +54,15 @@ describe Welcome do
     it "should be successful" do
       @controller.should be_successful
     end
-    
-    it "should have the latest 4 screencasts" do
-      @controller.assigns(:screencasts).should == @sc_result      
+
+    it "should have the latest 3 news items" do
+      @controller.assigns(:news_items).should == @news_items
+      @controller.assigns(:news_items).should have(3).items
     end
     
-    it "should have the latest 4 News Items" do
-      @controller.assigns(:news_items).should == @ni_result      
-    end
-    
-    it "should have the latest 4 tutorials""" do
-      @controller.assigns(:tutorials).should == @tut_result
+    it "should have the latest 5 items" do
+      @controller.assigns(:recent_items).should == @recent_items
+      @controller.assigns(:recent_items).should have(5).items
     end
     
     it "should set @feed_items if a feed is requested" do
