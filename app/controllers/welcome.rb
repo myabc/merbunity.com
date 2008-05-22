27bf8bfddb3c1c16effb790c878a5ba158fdea43
Daniel Neighman <has.sox@gmail.com>
2008-05-23 01:30:11 +1000

    Changes around the welcome page to seperate news, but mixin tutorials and screencasts

--- app/controllers/welcome.rb
+++ app/controllers/welcome.rb
@@ -7,11 +7,11 @@ class Welcome < Application
       @feed_items = [Screencast.published(:limit => 10), Tutorial.published(:limit => 10), NewsItem.all(:limit => 10)].flatten.compact
       @feed_items = @feed_items.sort_by{|item| item.respond_to?(:published_on) ? item.published_on : item.created_at }.reverse
       @feed_items = @feed_items[0,10]
-      puts @feed_items.map{|i| i.title}.inspect
     else
-      @screencasts  = Screencast.published(  :limit => 4)
-      @news_items   = NewsItem.all(          :limit => 4)
-      @tutorials    = Tutorial.published(     :limit => 4)
+      @news_items   = NewsItem.all( :limit => 3, :order => "created_at DESC")
+      @recent_items = [Screencast.published(:limit => 5), Tutorial.published(:limit => 5)].flatten.compact
+      @recent_items = @recent_items.sort_by{|i| i.published_on}.reverse
+      @recent_items = @recent_items[0,5]
     end
 
     render
