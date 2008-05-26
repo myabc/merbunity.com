1781190fe7a5f065711dee1ea4b0bb6ab85c15df
Daniel Neighman <has.sox@gmail.com>
2008-05-27 02:33:56 +1000

    Stuf

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,25 +1,23 @@
-unless defined?(Tutorial)  # WTF DO I HAVE TO TEST THIS!!!
-  class Tutorial
-    include DataMapper::Resource
-    include Merbunity::WhistlerHelpers::DataMapper
+class Tutorial
+  include DataMapper::Resource
+  include Merbunity::WhistlerHelpers::DataMapper
 
-    include Merbunity::Permissions::ProtectedModel
-    include Merbunity::Publishable
+  include Merbunity::Permissions::ProtectedModel
+  include Merbunity::Publishable
 
-    is_commentable :published, :pending
+  is_commentable :published, :pending
 
-    property :id,                       Integer,                  :serial => true
-    property :title,                    String,                   :nullable => false
-    property :description,              String,                   :nullable => false
-    property :body,                     DataMapper::Types::Text,  :nullable => false
+  property :id,                       Integer,                  :serial => true
+  property :title,                    String,                   :nullable => false
+  property :description,              String,                   :nullable => false
+  property :body,                     DataMapper::Types::Text,  :nullable => false
 
-    whistler_properties :title, :body, :description
+  whistler_properties :title, :body, :description
 
-    # validates_present :title, :description, :body
+  # validates_present :title, :description, :body
 
-    def display_body
-      return "" if self.body.nil?
-      @_display_body ||= RedCloth.new(self.body.gsub(/<code.*?<\/code>/mi){|s| s.gsub(/&lt;/,"<")}).to_html
-    end
+  def display_body
+    return "" if self.body.nil?
+    @_display_body ||= RedCloth.new(self.body.gsub(/<code.*?<\/code>/mi){|s| s.gsub(/&lt;/,"<")}).to_html
   end
 end
