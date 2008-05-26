b99889d7f6a929b50876af6d3b73c78951270bf9
Yehuda Katz <wycats@gmail.com>
2008-05-26 09:23:02 -0700

    Fix double-load

--- app/controllers/tutorials.rb
+++ app/controllers/tutorials.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '../models/tutorial')
+# require File.join(File.dirname(__FILE__), '../models/tutorial')
 class Tutorials < Application
   publishable_resource Tutorial
   params_protected :tutorial => [:owner, :owner_id, :published_on, :draft_status]
