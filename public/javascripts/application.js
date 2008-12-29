f3338851dbb215addf59bbbba3df5c0441774de8
Luke Sutton <lukesutton@wizzfizz.local>
2008-12-29 20:50:17 +1030

    Add cycling to the frontpage highlights.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,37 @@
-// Common JavaScript code across your application goes here.
+$.fn.cycleHighlights = function() {
+  var WIDTH = "970px";
+  var FREQUENCY = 6000;
+  
+  var ul = $(this);
+  var entries = ul.find("li");
+  ul.find("li:not(:eq(0))").css({left: WIDTH});
+  
+  var current = $(entries[0]);
+  var currentIndex = 0;
+  var next = null;
+  var nextIndex = null;
+  var total = entries.length - 1;
+  
+  // Cycles through each of the features
+  var cycle = function() {
+    nextIndex = (currentIndex < total ? currentIndex + 1 : 0);
+    next = $(entries[nextIndex]);
+    current.css({"z-index": 1});
+    next.css({"z-index": 2});
+    next.animate({left: "0px"}, 600, "linear", hideCurrent);
+    window.setTimeout(cycle, FREQUENCY);
+  };
+  
+  var hideCurrent = function() {
+    current.css({left: WIDTH});
+    current = next;
+    currentIndex = nextIndex;
+  };
+  
+  window.setTimeout(cycle, FREQUENCY);
+};
+
+$(document).ready(function() {
+  $("body").addClass("javascript");
+  $("#home ul.highlights").cycleHighlights();
+});
