fa47534c973b4f6a79ce9e348eedfc3a5cb5380a
Luke Sutton <lukesutton@wizzfizz.local>
2008-12-31 16:28:55 +1030

    Add helper to highlight the nav properly.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,5 +1,13 @@
 module Merb
   module GlobalHelpers
-    # helpers defined here available to all views.  
+    # helpers defined here available to all views. 
+    def main_nav_link(text, url, opts = {})
+      if url == "/"
+        opts[:class] = "current" if request.env["REQUEST_PATH"] == url
+      elsif request.env["REQUEST_PATH"].match(%r{^#{url}})
+        opts[:class] = "current"
+      end
+      link_to(text, url, opts)
+    end
   end
 end
